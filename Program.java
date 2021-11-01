99566163d16c543572028032759d75d94e5e5649
Aarya0508 <83182583+Aarya0508@users.noreply.github.com>
2021-11-01 16:42:47 +0530

    Update Program.java

--- Program.java
+++ Program.java
@@ -3,7 +3,7 @@ class Program {
 static double sum(int x, int n) {
 double i, total = 1.0;
 for (i = 2; i <= n; i++)
-total = total + (Math.pow(x, i) *(i/ i));
+total = total + (Math.pow(x, i) /i);
 return total;
 }
 public static void main(String[] args) {
